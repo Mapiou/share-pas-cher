453fa243f8f5042eb9383ff356a412027e335dfc
Marion Lambert <marionlambert85@googlemail.com>
2018-11-28 15:47:29 +0100

    Categories added in the form to add new thing

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,4 +1,6 @@
 class Thing < ApplicationRecord
+  CATEGORY_LIST = ['Outillage & Travaux', 'Matériel de jardin', 'Electroménager', 'Maison & Déco', 'Evénement', 'High Tech']
+
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
@@ -7,4 +9,5 @@ class Thing < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   validates :title, presence: true
+  validates :category, inclusion: { in: Thing::CATEGORY_LIST }
 end
